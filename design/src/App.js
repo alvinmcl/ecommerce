41151ab706cc1b009f32c2305acef4919c3c7c8d
alvinmcl <82399813+alvinmcl@users.noreply.github.com>
2022-06-23 14:09:33 +0800

    initial commit

---
+++ design/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">ecommerce</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
