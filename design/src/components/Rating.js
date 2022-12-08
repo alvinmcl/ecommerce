c12a91f7eb5964a5223a55071d4e7051d5059ec1
alvinmcl <82399813+alvinmcl@users.noreply.github.com>
2022-12-08 11:51:04 +0800

    added rating and review and change product div to bootstrap card

---
+++ design/src/components/Rating.js
@@ -0,0 +1,37 @@
+function Rating(props) {
+  const { rating, numReviews } = props;
+  var hasHalfStar = false;
+  let targetedStarForHalf = -1;
+  const ratingStars = [];
+  if (rating !== undefined) {
+    let tempRating = new String(rating);
+    if (tempRating.includes('.')) {
+      hasHalfStar = tempRating.split('.')[1] >= 0.5;
+    }
+    targetedStarForHalf = parseInt(tempRating.split('.')[0]);
+
+    for (let i = 0; i < 5; i++) {
+      ratingStars.push(
+        <span>
+          <i
+            className={
+              i < targetedStarForHalf
+                ? 'fas fa-star'
+                : targetedStarForHalf === i && hasHalfStar
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          />
+        </span>
+      );
+    }
+  }
+
+  return (
+    <div className="rating">
+      {ratingStars}
+      <span> {numReviews} reviews</span>
+    </div>
+  );
+}
+export default Rating;
