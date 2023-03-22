5167de2e0213d11c7142307b7f0d60cf9286f764
alvinmcl <82399813+alvinmcl@users.noreply.github.com>
2023-03-22 17:08:41 +0800

    -test 4

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/build')));
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/build/index.html'))
